bb4cfdda5d3a78e99902055e6c4a2d5862e2e2ad
Pier-Alexis <pieralexiscarvalho@outlook.com>
2025-01-16 09:45:54 -0500

    added interrupts

--- kernel/kernel.asm
+++ kernel/kernel.asm
@@ -1,8 +1,23 @@
 bits 32
 
+section .data
+idt_pointer:
+    dw 256 * 8 - 1       ; Taille de l'IDT
+    dd idt_table         ; Adresse de l'IDT
+
+section .bss
+align 8
+idt_table resb 256 * 8  ; 256 entrées, 8 octets par entrée
+
 section .text
-global start
+global start, load_idt
 
 start:
-    mov eax, 0x1
+    ; Initialisation du noyau
+    call load_idt
     hlt
+
+load_idt:
+    ; Charger le registre IDT
+    lidt [idt_pointer]
+    ret
