5f5b49e76f5afeef441d47b21020b3ff5edebeae
Senka107 <93744556+Pier-Alexis@users.noreply.github.com>
2025-01-19 19:44:08 -0500

    Update bootloader.asm

--- bootloader/bootloader.asm
+++ bootloader/bootloader.asm
@@ -29,12 +29,22 @@ start:
     mov al, 0x0
     out 0x21, al
 
-    ; Vérification de la mémoire
-    mov ah, 0x00
-    int 0x12
-    cmp ax, 0x0000
-    je load_error
+    ; Afficher le menu de démarrage
+    mov si, boot_menu
+    call print_string
 
+    ; Lire la sélection de l'utilisateur
+    mov ah, 0x00
+    int 0x16
+
+    ; Vérifier la sélection
+    cmp al, '1'
+    je load_stage2
+    cmp al, '2'
+    je load_recovery
+    jmp hang
+
+load_stage2:
     ; Charger le stage2
     mov bx, 0x2000
     mov dh, 1
@@ -47,6 +57,20 @@ start:
 
     ; Sauter à stage2
     jmp 0x2000:0x0000
+
+load_recovery:
+    ; Charger le mode de récupération
+    mov bx, 0x3000
+    mov dh, 1
+    mov dl, 0
+    mov ch, 0
+    mov cl, 3
+    mov ah, 0x02
+    int 0x13
+    jc load_error
+
+    ; Sauter au mode de récupération
+    jmp 0x3000:0x0000
 
 load_error:
     ; Afficher un message d'erreur
@@ -63,7 +87,18 @@ hang:
     hlt
     jmp hang
 
-err_msg db 'Erreur de chargement du stage2', 0
+boot_menu db '1. Démarrer le système', 0x0D, 0x0A, '2. Mode de récupération', 0x0D, 0x0A, 'Choisissez une option: ', 0
+err_msg db 'Erreur de chargement', 0
+
+print_string:
+    lodsb
+    cmp al, 0
+    je done
+    mov ah, 0x0E
+    int 0x10
+    jmp print_string
+done:
+    ret
 
 times 510-($-$$) db 0
 dw 0xAA55
