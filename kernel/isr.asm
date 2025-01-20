fef24b33841b5c00d942531ad59399b78c93350d
Senka107 <93744556+Pier-Alexis@users.noreply.github.com>
2025-01-19 19:38:43 -0500

    Update isr.asm

--- kernel/isr.asm
+++ kernel/isr.asm
@@ -21,4 +21,128 @@ isr1:
     sti
     iret
 
-; Répétez pour les autres ISR (isr2, isr3, ...)
+isr2:
+    cli
+    pusha
+    mov al, '2'
+    out 0xE9, al
+    popa
+    sti
+    iret
+
+isr3:
+    cli
+    pusha
+    mov al, '3'
+    out 0xE9, al
+    popa
+    sti
+    iret
+
+isr4:
+    cli
+    pusha
+    mov al, '4'
+    out 0xE9, al
+    popa
+    sti
+    iret
+
+isr5:
+    cli
+    pusha
+    mov al, '5'
+    out 0xE9, al
+    popa
+    sti
+    iret
+
+isr6:
+    cli
+    pusha
+    mov al, '6'
+    out 0xE9, al
+    popa
+    sti
+    iret
+
+isr7:
+    cli
+    pusha
+    mov al, '7'
+    out 0xE9, al
+    popa
+    sti
+    iret
+
+isr8:
+    cli
+    pusha
+    mov al, '8'
+    out 0xE9, al
+    popa
+    sti
+    iret
+
+isr9:
+    cli
+    pusha
+    mov al, '9'
+    out 0xE9, al
+    popa
+    sti
+    iret
+
+isr10:
+    cli
+    pusha
+    mov al, 'A'
+    out 0xE9, al
+    popa
+    sti
+    iret
+
+isr11:
+    cli
+    pusha
+    mov al, 'B'
+    out 0xE9, al
+    popa
+    sti
+    iret
+
+isr12:
+    cli
+    pusha
+    mov al, 'C'
+    out 0xE9, al
+    popa
+    sti
+    iret
+
+isr13:
+    cli
+    pusha
+    mov al, 'D'
+    out 0xE9, al
+    popa
+    sti
+    iret
+
+isr14:
+    cli
+    pusha
+    mov al, 'E'
+    out 0xE9, al
+    popa
+    sti
+    iret
+
+isr15:
+    cli
+    pusha
+    mov al, 'F'
+    out 0xE9, al
+    popa
+    sti
+    iret
